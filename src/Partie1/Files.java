53bc0d856ad662bc964b7b20adc64d561329dcba
Brian <Brian@172.17.2.2>
2017-10-26 18:28:45 +0200

    Fin Partie1

--- src/Partie1/Files.java
+++ src/Partie1/Files.java
@@ -3,8 +3,8 @@ package Partie1;
 public interface Files <T>{
 	
 	public void ajout(T dernierDansFile);
+	public void retrait();
 	public T tete();
-	public void retrait(); // retrait de la tête
 	public String afficher();
 	public int size();
 

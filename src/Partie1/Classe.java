b4a0657a361573140fa7ef1b7ce6e63e4430675d
Brian <Brian@172.17.2.2>
2017-10-26 14:47:34 +0200

    coucou

--- src/Partie1/Classe.java
+++ src/Partie1/Classe.java
@@ -4,6 +4,7 @@ public class Classe {
 
 	public static void main(String[] args) {
 		System.out.println("Je suis une merde");
+		System.out.println("mais c'est rien à coté de Nessim Legendre");
 	}
 	
 }
